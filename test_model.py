8aa3ea199cad44ae0da9e15a771dc18add0c4337
Suhail Najeeb <suhailnajeeb19@gmail.com>
2021-05-06 05:21:47 +0600

    model testing script

--- test_model.py
+++ test_model.py
@@ -13,9 +13,13 @@ backbone = EfficientNet.from_pretrained('efficientnet-b4')
 backbone.source_layer_indexes = [21, 29]
 
 anchors = Anchors()
-anchors.strides = [2, 4, 8, 16, 32]
-anchors.sizes = [8, 16, 32, 64, 128]
 anchors.ratios = np.array([0.8, 1, 1.2])
+anchors.scales = np.array([0.375, 0.6875, 1.3125])
+
+pret_path = "models/efn64.pt"
+
+backbone = EfficientNet.from_pretrained_model('efficientnet-b4', pret_path)
+backbone.source_layer_indexes = [21, 29]
 
 model = RetinaNet(num_classes = 8, backbone_network = backbone, fpn_sizes = [160, 272, 1792], anchors = anchors)
 model.to(device)
